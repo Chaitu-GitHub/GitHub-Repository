c357b43f4b58e6a892be35571658ac356b432e72
chaitu <peddireddychaitanya99@gmail.com>
2018-12-13 13:16:28 +0530

    Commit2:Program Modified

--- Devops.java
+++ Devops.java
@@ -5,5 +5,12 @@ class HelloWorld
     public static void main(String args[]) 
     { 
         System.out.println("Hello, World"); 
-    } 
+ 
+    
+   } 
+
+   public static void main(String args[])
+    {
+        System.out.println("Welcom to Devops");
+    }
 } 
